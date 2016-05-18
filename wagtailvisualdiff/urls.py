5a000b160e52a8ff9e50ad8b15ebf3b944b91396
Adrian Goedeckemeyer <adrian@minerva.kgi.edu>
2016-05-17 17:17:57 -0700

    Fix name problem

--- wagtailvisualdiff/urls.py
+++ wagtailvisualdiff/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
 	# ex: /wagtailvisualdiff/
 	url(r'^$', views.index, name='index'),
 	# ex: /wagtailvisualdiff/1234/to_current/
-	url(r'^(?P<revision_id>[0-9]+)/to_current/$', views.visual_diff_current, name='diff'),
+	url(r'^(?P<revision_id>[0-9]+)/to_current/$', views.visual_diff_current, name='diff_current'),
 	# ex: /wagtailvisualdiff/1234/to_previous/
-	url(r'^(?P<revision_id>[0-9]+)/to_previous/$', views.visual_diff_previous, name='diff'),
+	url(r'^(?P<revision_id>[0-9]+)/to_previous/$', views.visual_diff_previous, name='diff_previous'),
 	# ex: /wagtailvisualdiff/1234/for_sc
 	url(r'^(?P<revision_id>[0-9]+)/for_sc/$', views.visual_diff_only, name='diff_only'),
 ]
